89d7236d5f43df834c838e307ec84b91eb647878
Stéphane Mourey <steph@stephanemourey.com>
2017-03-17 17:28:04 +0100

    Playing with time and display

--- www/js/index.js
+++ www/js/index.js
@@ -1,46 +1,61 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+	// Application Constructor
+	initialize: function() {
+		app.updateTimer = function(){
+			seconds = app.pomRemainingTime/1000;
+			app.minutes = Math.floor(seconds/60);
+			app.seconds = seconds-app.minutes*60;
+		};
+		app.updateTimerUI = function(){
+			$('#chrono').html(app.minutes+':'+app.seconds);
+		};
+		app.countdown = function(){
+			app.pomRemainingTime-=1000;
+			app.updateTimer();
+			app.updateTimerUI();
+		};
+		$('#newBtn').click(function(e){
+			app.pomState = 'running';
+			app.pomRemainingTime = 25*60*1000;
+			app.countdowner = setInterval(app.countdown,1000);	
+		});
+		$('#shortPauseBtn').click(function(e){
+			console.log(e.currentTarget.id); 	
+		});
+		$('#longPauseBtn').click(function(e){
+			console.log(e.currentTarget.id); 	
+		});
+		$('#interruptBtn').click(function(e){
+			console.log(e.currentTarget.id); 	
+		});
+		$('#restartBtn').click(function(e){
+			console.log(e.currentTarget.id); 	
+		});
+		$('#parametersBtn').click(function(e){
+			console.log(e.currentTarget.id); 	
+		});
+			//document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+	},
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
+	// deviceready Event Handler
+	//
+	// Bind any cordova events here. Common events are:
+	// 'pause', 'resume', etc.
+	//onDeviceReady: function() {
+	//    this.receivedEvent('deviceready');
+	//~ },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+	// Update DOM on a Received Event
+	//~ receivedEvent: function(id) {
+			//~ var parentElement = document.getElementById(id);
+			//~ var listeningElement = parentElement.querySelector('.listening');
+			//~ var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+			//~ listeningElement.setAttribute('style', 'display:none;');
+			//~ receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
+			//~ console.log('Received Event: ' + id);
+	//~ }
 };
 
 app.initialize();
