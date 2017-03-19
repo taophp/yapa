fcdc12f4cbc10a8cce39a7d3348052b83d298d2f
Stéphane Mourey <stephane.mourey@impossible-exil.info>
2017-03-19 17:48:42 +0100

    Add some actions to buttons

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# yapa
-Yet Another Pomodoro App
+# YaPa
+Yet another Pomodoro app
+
+YaPa is a small project, just to make my teeth on the Cordova framework.
+
+## Setup
+
+This is a [Cordova](https://cordova.apache.org) project. You need to install Cordova and its dependencies to compile the project.
+
+Once done, clone the source in your local filessystem. In the project directory, add the platforms you need :
+
+	$ cordova platform add browser ubuntu
+
+Then add the required plugins :
+
+	$ cordova plugin add cordova-plugin-dialogs cordova-plugin-vibration
+
+## Todo
+
+* add some sort of indication when the counter is interrupted
+* make the apple become more and more red as the time passes
+* manage the workflow
+* the configuration screen
--- www/js/index.js
+++ www/js/index.js
@@ -1,61 +1,65 @@
 var app = {
 	// Application Constructor
 	initialize: function() {
+		app.states = {
+			'pomodoro': {'length':25*60*1000,'displayedStatus':'Work hard now !'},
+			'shortPause': {'length':5*60*1000,'displayedStatus':'You deserve a short break...'},
+			'longPause': {'length':15*60*1000,'displayedStatus':'You deserve a long break...'}
+		};
 		app.updateTimer = function(){
-			seconds = app.pomRemainingTime/1000;
+			seconds = app.remainingTime/1000;
 			app.minutes = Math.floor(seconds/60);
 			app.seconds = seconds-app.minutes*60;
 		};
 		app.updateTimerUI = function(){
-			$('#chrono').html(app.minutes+':'+app.seconds);
+			seconds = (app.seconds.toString().length>1) ? app.seconds : '0'+app.seconds;
+			$('#chrono').html(app.minutes+':'+seconds);
+			$('#status').html(app.states[app.state]['displayedStatus']);
 		};
 		app.countdown = function(){
-			app.pomRemainingTime-=1000;
+			app.remainingTime-=1000;
 			app.updateTimer();
 			app.updateTimerUI();
+			if (app.remainingTime<=0){
+				app.timeends();
+			}
 		};
-		$('#newBtn').click(function(e){
-			app.pomState = 'running';
-			app.pomRemainingTime = 25*60*1000;
-			app.countdowner = setInterval(app.countdown,1000);	
-		});
-		$('#shortPauseBtn').click(function(e){
-			console.log(e.currentTarget.id); 	
-		});
-		$('#longPauseBtn').click(function(e){
-			console.log(e.currentTarget.id); 	
-		});
+		app.timeends = function(){
+			window.clearInterval(app.countdowner);
+			app.countdowner= false;
+			navigator.vibrate(200);
+			navigator.notification.beep(1);
+			navigator.notification.alert('Time ends... what to do next ?');
+		};
+		app.changeState = function(state){
+			app.state = state;
+			window.clearInterval(app.countdowner);
+			app.remainingTime = app.states[state]['length'];
+			app.updateTimer();
+			app.updateTimerUI();
+			app.countdowner = setInterval(app.countdown,1000);
+		};
+		$('#newBtn').click(function(){app.changeState('pomodoro');});
+		$('#shortPauseBtn').click(function(){app.changeState('shortPause');});
+		$('#longPauseBtn').click(function(){app.changeState('longPause');});
 		$('#interruptBtn').click(function(e){
-			console.log(e.currentTarget.id); 	
+			$('#interruptBtn > span').toggleClass('glyphicon-pause glyphicon-play');
+			if (app.countdowner) {
+				window.clearInterval(app.countdowner);
+				app.countdowner= false;
+			}else{
+				app.countdowner = setInterval(app.countdown,1000);
+			}
 		});
 		$('#restartBtn').click(function(e){
-			console.log(e.currentTarget.id); 	
+			app.remainingTime = app.states[app.state]['length'];
+			app.updateTimer();
+			app.updateTimerUI();
 		});
 		$('#parametersBtn').click(function(e){
-			console.log(e.currentTarget.id); 	
+			navigator.notification.alert('Not yet implemented...',function(){},'WTF?!?');
 		});
-			//document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
 	},
-
-	// deviceready Event Handler
-	//
-	// Bind any cordova events here. Common events are:
-	// 'pause', 'resume', etc.
-	//onDeviceReady: function() {
-	//    this.receivedEvent('deviceready');
-	//~ },
-
-	// Update DOM on a Received Event
-	//~ receivedEvent: function(id) {
-			//~ var parentElement = document.getElementById(id);
-			//~ var listeningElement = parentElement.querySelector('.listening');
-			//~ var receivedElement = parentElement.querySelector('.received');
-
-			//~ listeningElement.setAttribute('style', 'display:none;');
-			//~ receivedElement.setAttribute('style', 'display:block;');
-
-			//~ console.log('Received Event: ' + id);
-	//~ }
 };
 
 app.initialize();
